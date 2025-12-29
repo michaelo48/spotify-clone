4558d076c0c0929f60d863ccb966cd58cd2050c4
michaelo48 <michaelo48@vt.edu>
2025-12-29 14:30:21 -0500

    added scroll

--- js/sidebar.js
+++ js/sidebar.js
@@ -79,6 +79,7 @@ async function getSpotifyPlaylists() {
             fetch('https://api.spotify.com/v1/me/player/recently-played?limit=50', {
                 headers: { 'Authorization': `Bearer ${accessToken}` }
             })
+            
         ]);
 
         console.log('Playlists response status:', playlistsResponse.status);
